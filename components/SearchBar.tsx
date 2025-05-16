d77577f5d9e83b0c0a39bd4e678ffa48bc796a28
RABARI KRISH <rabarikrishramjibhai2905@gmail.com>
2025-05-16 17:47:08 +0530

    Upto HomeScreen UI

---
+++ components/SearchBar.tsx
@@ -0,0 +1,28 @@
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+import { icons } from '@/constants/icons'
+
+interface Props {
+  placeHolder: string,
+  onPress ? ():void;
+}
+
+const SearchBar = ({placeHolder,onPress}:Props) => {
+  return (
+    <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: '#333', borderRadius: 25, paddingHorizontal: 20, paddingVertical: 10 }}>
+      <Image source={icons.search} style={{ width: 20, height: 20, resizeMode: 'contain', tintColor: '#ab8ff' }} />
+      <TextInput 
+      onPress={onPress}
+      placeholder = {placeHolder}
+      value=''
+      onChangeText={()=>{}}
+      placeholderTextColor="#a8b5db"
+      className='flex-1 ml-2 text-white'
+      />
+    </View>
+  )
+}
+
+export default SearchBar
+
+const styles = StyleSheet.create({})
