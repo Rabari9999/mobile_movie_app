c2e95cb5e855b10e74fdf4a4b92a89feccb35641
RABARI KRISH <rabarikrishramjibhai2905@gmail.com>
2025-05-18 01:02:59 +0530

    Appwrite set-up

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -6,6 +6,7 @@ import useFetch from '@/services/useFetch'
 import { fetchMovies } from '@/services/api'
 import { icons } from '@/constants/icons'
 import SearchBar from '@/components/SearchBar'
+import { updateSearchCount } from '@/services/appwrite'
 
 const search = () => {
 
@@ -22,6 +23,10 @@ const[searchQuery,setSearchQuery] = useState('');
       const debouncing = setTimeout(async() =>{
         if(searchQuery.trim()){
           await loadMovies();
+
+           if(movies?.length>0 && movies?.[0]){
+              await updateSearchCount(searchQuery,movies[0]);
+           }
         }
         else{
           reset()
